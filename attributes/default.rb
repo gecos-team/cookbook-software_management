a7fa328439f3cfed8acae0e61f8a8eb71831591c
Roberto C. Morano <rcmorano@emergya.com>
2011-12-13 18:07:20 +0100

    added pkg groups

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,18 +3,71 @@ default[:packages][:to_install] = []
 default[:packages][:to_remove] = []
 
 # List of group of packages
-default[:groups][:to_install] = [ :basic, :office]
-default[:groups][:to_remove] = [ :basic, :office]
+default[:groups][:to_install] = [ :office, :mail]
+default[:groups][:to_remove] = [ :office, :mail]
 
 # Definition of the groups of packages
-default[:groups][:basic] = []
 default[:groups][:office] = [
-                             'libreoffice-writer',
-                             'libreoffice-calc',
-                             'libreoffice-impress'
+                              'libreoffice',
+                              'libreoffice-gnome',
+                              'libreoffice-base',
+                              'libreoffice-calc',
+                              'libreoffice-draw',
+                              'libreoffice-impress',
+                              'libreoffice-math',
+                              'libreoffice-writer',
+                              'libreoffice-pdfimport',
+                              'libreoffice-emailmerge',
+                              'libreoffice-gtk',
+                              'libreoffice-style-tango',
+                              'libreoffice-l10n-es',
+                              'libreoffice-help-es',
+                              'freemind',
+                              'tomboy'
                             ]
-default[:groups][:internet] = [
-                               'firefox',
-                               'firefox-firma'
+
+default[:groups][:mail] = [
+                            'thunderbird-locale-en-gb',
+                            'thunderbird-locale-en-us',
+                            'thunderbird-locale-es-es',
+                            'thunderbird',
+                            'thunderbird-gnome-support'
+                          ]
+
+default[:groups][:pdftools] = [
+                                'pdfmod'
                               ]
+
+default[:groups][:imgscan] = [
+                               'simple-scan',
+                               'tesseract-ocr-spa',
+                               'tesseract-ocr'
+                             ]
+
+default[:groups][:imgedit] = [
+                               'gimp',
+                               'gimp-help-es',
+                               'gimp-help-en',
+                               'inkscape'
+                             ]
+
+default[:groups][:remoteadmin] = [
+                                   'vnc4server'
+                                 ]
+
+default[:groups][:collaborative] = [
+                                     'pidgin',
+                                     'gobby'
+                                   ]
+default[:groups][:a11y] = [
+                            'gnome-orca',
+                            'caribou'
+                          ]
+
+default[:groups][:tablet] = [
+                              'xournal',
+                              'easystroke',
+                              'xrandr',
+                              'cellwriter'
+                            ]
 
