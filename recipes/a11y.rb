653e2ad1b4025f41e0fa3e7cd4d7eb278e0c0cf5
Roberto C. Morano <rcmorano@emergya.com>
2012-01-11 14:08:08 +0100

    added individual recipes for each packages list

---
+++ recipes/a11y.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: add_remove_software
+# Recipe:: a11y
+#
+# Copyright 2011, Junta de Andalucía
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if node["add_remove_software"]["groups"]["a11y"]["install"] == "yes"
+
+  add_remove_software_package_list "a11y" do
+    packages_to_install node["add_remove_software"]["groups"]["a11y"]["packages"]
+    action :process
+  end
+
+elsif node["add_remove_software"]["groups"]["a11y"]["install"] == "no"
+
+  add_remove_software_package_list "a11y" do
+    packages_to_remove node["add_remove_software"]["groups"]["a11y"]["packages"]
+    action :process
+  end
+
+end
