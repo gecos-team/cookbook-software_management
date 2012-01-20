bb159f458d848c762d31c80914d1fc7e190fbc75
Roberto C. Morano <rcmorano@emergya.com>
2012-01-20 12:00:39 +0100

    fixed lil typo

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -19,6 +19,6 @@
 
 add_remove_software_package_list "packages" do
   packages_to_install node["packages"]["to_install"]
-  packages_to_remove node"packages"]["to_remove"]
+  packages_to_remove node["packages"]["to_remove"]
   action :process
 end
