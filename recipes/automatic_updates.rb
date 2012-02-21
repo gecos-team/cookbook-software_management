da976d724c0e9f18755dbe414f66daa2c157e851
Roberto C. Morano <rcmorano@emergya.com>
2012-02-21 14:39:04 +0100

    removed unneeded check that was causing malfunction

--- recipes/automatic_updates.rb
+++ recipes/automatic_updates.rb
@@ -48,118 +48,107 @@ else
 end
 
 
-if node["automatic_updates"]["on_monday"] == "on" or node["automatic_updates"]["on_tuesday"] == "on" or
-node["automatic_updates"]["on_wednesday"] == "on" or node["automatic_updates"]["on_thursday"] == "on" or
-node["automatic_updates"]["on_friday"] == "on"
-  package 'cron-apt' do
-    action :nothing
-  end.run_action(:install)
-  
-  cron_apt_config = "/etc/cron-apt/config"
-  cookbook_file cron_apt_config do
-    source "apt.config"
-    owner "root"
-    group "root"
-    mode "0644"
-  end
-  cron_apt_action_update = "/etc/cron-apt/action.d/0-update"
-  cookbook_file cron_apt_action_update do
-    source "apt_action.update"
-    owner "root"
-    group "root"
-    mode "0644"
-  end
+package 'cron-apt' do
+  action :nothing
+end.run_action(:install)
 
-  cron_apt_action_upgrade = "/etc/cron-apt/action.d/3-download"
-  cookbook_file cron_apt_action_upgrade do
-    source "apt_action.upgrade"
-    owner "root"
-    group "root"
-    mode "0644"
-  end
- 
-  if node["automatic_updates"]["max_random_time_span"] != "000"
-    time_span = node["automatic_updates"]["max_random_time_span"].to_i
-    span_hour, span_min = time_span.divmod(60)
-  else
-    span_hour, span_min = 0
-  end
+cron_apt_config = "/etc/cron-apt/config"
+cookbook_file cron_apt_config do
+  source "apt.config"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+cron_apt_action_update = "/etc/cron-apt/action.d/0-update"
+cookbook_file cron_apt_action_update do
+  source "apt_action.update"
+  owner "root"
+  group "root"
+  mode "0644"
+end
 
- 
-  if node["automatic_updates"]["on_monday"] == "on"
-    hour, min = node["automatic_updates"]["on_monday_time"].split(":")
-    mon_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    mon_time = "#"
-  end
+cron_apt_action_upgrade = "/etc/cron-apt/action.d/3-download"
+cookbook_file cron_apt_action_upgrade do
+  source "apt_action.upgrade"
+  owner "root"
+  group "root"
+  mode "0644"
+end
 
-  if node["automatic_updates"]["on_tuesday"] == "on"
-    hour, min = node["automatic_updates"]["on_tuesday_time"].split(":")
-    tue_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    tue_time = "#"
-  end
-
-  if node["automatic_updates"]["on_wednesday"] == "on"
-    hour, min = node["automatic_updates"]["on_wednesday_time"].split(":")
-    wed_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    wed_time = "#"
-  end
-
-
-  if node["automatic_updates"]["on_thursday"] == "on"
-    hour, min = node["automatic_updates"]["on_thursday_time"].split(":")
-    thu_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    thu_time = "#"
-  end
-
-
-  if node["automatic_updates"]["on_friday"] == "on"
-    hour, min = node["automatic_updates"]["on_friday_time"].split(":")
-    fri_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    fri_time = "#"
-  end
-
-
-  if node["automatic_updates"]["on_saturday"] == "on"
-    hour, min = node["automatic_updates"]["on_saturday_time"].split(":")
-    sat_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    sat_time = "#"
-  end
-
-
-  if node["automatic_updates"]["on_sunday"] == "on"
-    hour, min = node["automatic_updates"]["on_sunday_time"].split(":")
-    sun_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
-  else
-    sun_time = "#"
-  end
-
-
-
-  cron_apt = "/etc/cron.d/cron-apt"
-  template cron_apt do
-    owner "root"
-    group "root"
-    mode "0644"
-    variables (:mon_time => mon_time,
-               :tue_time => tue_time,
-               :wed_time => wed_time,
-               :thu_time => thu_time,
-               :fri_time => fri_time,
-               :sat_time => sat_time,
-               :sun_time => sun_time)
-    source "cron-apt.erb"
-  end
-
-
+if node["automatic_updates"]["max_random_time_span"] != "000"
+  time_span = node["automatic_updates"]["max_random_time_span"].to_i
+  span_hour, span_min = time_span.divmod(60)
 else
-  package 'cron-apt' do
-    action :nothing
-  end.run_action(:remove)
+  span_hour, span_min = 0
+end
 
+
+if node["automatic_updates"]["on_monday"] == "on"
+  hour, min = node["automatic_updates"]["on_monday_time"].split(":")
+  mon_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  mon_time = "#"
+end
+
+if node["automatic_updates"]["on_tuesday"] == "on"
+  hour, min = node["automatic_updates"]["on_tuesday_time"].split(":")
+  tue_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  tue_time = "#"
+end
+
+if node["automatic_updates"]["on_wednesday"] == "on"
+  hour, min = node["automatic_updates"]["on_wednesday_time"].split(":")
+  wed_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  wed_time = "#"
+end
+
+
+if node["automatic_updates"]["on_thursday"] == "on"
+  hour, min = node["automatic_updates"]["on_thursday_time"].split(":")
+  thu_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  thu_time = "#"
+end
+
+
+if node["automatic_updates"]["on_friday"] == "on"
+  hour, min = node["automatic_updates"]["on_friday_time"].split(":")
+  fri_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  fri_time = "#"
+end
+
+
+if node["automatic_updates"]["on_saturday"] == "on"
+  hour, min = node["automatic_updates"]["on_saturday_time"].split(":")
+  sat_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  sat_time = "#"
+end
+
+
+if node["automatic_updates"]["on_sunday"] == "on"
+  hour, min = node["automatic_updates"]["on_sunday_time"].split(":")
+  sun_time = "#{min.to_i + span_min} #{hour.to_i + span_hour}"
+else
+  sun_time = "#"
+end
+
+
+
+cron_apt = "/etc/cron.d/cron-apt"
+template cron_apt do
+  owner "root"
+  group "root"
+  mode "0644"
+  variables (:mon_time => mon_time,
+             :tue_time => tue_time,
+             :wed_time => wed_time,
+             :thu_time => thu_time,
+             :fri_time => fri_time,
+             :sat_time => sat_time,
+             :sun_time => sun_time)
+  source "cron-apt.erb"
 end
